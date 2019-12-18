2206016744f884f2b456e9a30bdf2f54b03ad08c
cd <x19921111@gmail.com>
2019-12-18 16:11:45 +0800

    修改nav的連結、聯絡我們map大小

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -17,7 +17,7 @@
 								</ul>
           </div>
           <div class="col-md-7">
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3269.2032817046734!2d121.5613911667799!3d25.03888726183313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abba3fcafd03%3A0x14be4c5abaf47f42!2zQ2Fub24gSW1hZ2UgU3F1YXJl5b2i6LGh5qaC5b-15bqXKOaWsOWFieS4iei2ikE46aSo5LiJ5qiTKQ!5e0!3m2!1szh-TW!2stw!4v1576602761704!5m2!1szh-TW!2stw" width="800" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3269.2032817046734!2d121.5613911667799!3d25.03888726183313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abba3fcafd03%3A0x14be4c5abaf47f42!2zQ2Fub24gSW1hZ2UgU3F1YXJl5b2i6LGh5qaC5b-15bqXKOaWsOWFieS4iei2ikE46aSo5LiJ5qiTKQ!5e0!3m2!1szh-TW!2stw!4v1576602761704!5m2!1szh-TW!2stw" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 					</div>
 				</div>
 				<!-- /row -->
