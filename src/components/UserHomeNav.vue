3d4938fac08f295ede59a23303b29a1810047547
cd <x19921111@gmail.com>
2019-12-19 17:08:05 +0800

    新增購物車qty變化顯示

--- src/components/UserHomeNav.vue
+++ src/components/UserHomeNav.vue
@@ -74,7 +74,7 @@
 										 <router-link to="/cart" class="shopping-cart-RL">
 											<span>購物車</span>
 										 </router-link>
-										<div class="qty">10</div>
+										<div class="qty">{{cart.carts.length}}</div>
 									</a>
 									<div class="cart-dropdown">
 										<div class="cart-list">
@@ -141,10 +141,10 @@
 					<ul class="main-nav nav navbar-nav" style="-webkit-box-orient: horizontal;">
 						<li class="active"><a href="#">首頁</a></li>
 						<li><a href="/#/"><router-link to="/newcoupon">最新折扣</router-link></a></li>
-						<li><a href="#">專業數位單眼相機</a></li>
-						<li><a href="#">專業數位光學鏡頭</a></li>
-						<li><a href="#">專業閃光燈與配件</a></li>
-						<li><a href="/#/"><router-link to="/about">關於我們</router-link></a></li>
+						<li><a href="/#/">專業數位單眼相機</a></li>
+						<li><a href="/#/">專業數位光學鏡頭</a></li>
+						<li><a href="/#/">專業閃光燈與配件</a></li>
+g						<li><a href="/#/"><router-link to="/about">關於我們</router-link></a></li>
 						<li><a href="/#/"><router-link to="/contact">聯絡我們</router-link></a></li>
 						<!-- <li><a href="#">Accessories</a></li> -->
 					</ul>
@@ -162,4 +162,145 @@
 export default {
 	name: 'UserHomeNav',
 }
+</script>
+<script>
+import $ from 'jquery'; //載入jQuery
+import { ValidationProvider } from 'vee-validate';
+export default {
+  data() {
+    return {
+      products: [],
+			product: {},
+			pagination:[], //新增分頁
+      status: {
+        loadingItem: '', //判定畫面中何者元素正在讀取中
+      },
+      form:{
+        user:{
+           name:'',
+          email:'',
+          tel:'',
+          address:'',
+        },
+        message:'',
+      },
+			cart:{},
+			carts:[],
+      isLoading: false,
+      coupon_code:'',
+    };
+  },
+  methods: {
+    // getProducts() {
+    //   const vm = this;
+    //   const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    //   vm.isLoading = true;
+    //   this.$http.get(url).then((response) => {
+    //     vm.products = response.data.products;
+    //     console.log("getProducts取得全部產品資訊",response);
+    //     vm.isLoading = false;
+    //   });
+		// },
+		getProducts(page = 1){ //ES6: page的預設值為1
+        const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products?page=${page}`; //建立api的變數
+        const vm = this;
+        vm.isLoading = true; //啟動getProducts時啟用isLoading效果
+        this.$http.get(api).then((response) => {
+          console.log("getProducts:"+response.data);
+          // if(response.data.success){
+            vm.isLoading = false; //關閉getProducts時關閉isLoading效果
+            vm.products = response.data.products;
+            vm.pagination = response.data.pagination;
+          // }
+        });
+      },
+    getProduct(id) {
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+      vm.status.loadingItem = id;
+      this.$http.get(url).then((response) => {
+        vm.product = response.data.product;
+        $('#productModal').modal('show'); //打開modal
+        console.log("getProduct取得單一產品資訊",response);
+        vm.status.loadingItem = '';//讀取完成後，將loadingItem的id設為空值
+      });
+    },
+    addtoCart(id,qty = 1){
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      vm.status.loadingItem = id;
+      const cart = {
+        product_id : id,
+        qty,
+      };
+      this.$http.post(url, {data: cart} ).then((response) => {
+        $('#productModal').modal('hide'); //關閉modal
+				console.log("成功加入購物車",response);
+        vm.getCart();
+				vm.status.loadingItem = '';//讀取完成後，將loadingItem的id設為空值
+      });
+    },
+    getCart(){
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      vm.isLoading = true;
+      this.$http.get(url).then((response) => {
+				vm.cart = response.data.data;
+        console.log("取得購物車資訊",response);
+        vm.isLoading = false;
+      });
+		},
+    removeCartItem(id){
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart/${id}`;
+      vm.isLoading = true;
+      this.$http.delete(url).then((response) => { //使用delete行為
+        vm.getCart();
+        console.log("已刪除購物車中的內容",response);
+        vm.isLoading = false;
+      });
+    },
+    addCouponCode(){
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/coupon`;
+      const coupon = { //建立coupon變數對應couponCode的資料結構
+        code : vm.coupon_code,
+      };
+      vm.isLoading = true;
+      this.$http.post(url, {data:coupon}).then((response) => { //使用post行為送出data
+        vm.getCart();
+        console.log("已新增優惠券",response);
+        vm.isLoading = false;
+      });
+    },
+    createOrder(){
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/order`;
+      const order = vm.form;
+      vm.isLoading = true;
+       this.$validator.validate().then((result) => {
+        if (result) {
+          this.$http.post(url, { data: order }).then((response) => {
+            console.log('訂單已建立', response);
+            if(response.data.success){ //當訂單資料成功傳送時
+              vm.$router.push(`/customer_checkout/${response.data.orderId}`);//跳轉至customer_checkout頁面
+            };
+            vm.isLoading = false;
+          });
+        } else {
+          console.log('欄位不完整');
+        }
+      });
+      // this.$http.post(url, {data:order}).then((response) => { //使用post行為送出data
+      //   // vm.getCart();
+      //   console.log("訂單已成立",response);
+      //   vm.isLoading = false;
+      // });
+    },
+  },
+  created() {
+    this.getProducts();
+		this.getCart();
+  },
+};
 </script>
